772194147ba48656b5dd2a320ad3c431266837a2
Elcio Nakashima <elcio.nakashima@digitra.com>
2021-12-14 20:36:58 -0300

    Adding AWS Lambda cross account deploy policy

--- ecr-policies.tf
+++ ecr-policies.tf
@@ -37,6 +37,26 @@ resource "aws_ecr_repository_policy" "default" {
         "ecr:UploadLayerPart",
         "ecr:CompleteLayerUpload"
       ]
+    },
+    {
+      "Sid": "LambdaECRImageCrossAccountRetrievalPolicy",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": [
+          "lambda.amazonaws.com"
+        ]
+      },
+      "Action": [
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage"
+      ],
+      "Condition": {
+        "StringLike": {
+          "aws:sourceArn": [
+            ${join(",", formatlist("\"arn:aws:lambda:us-east-1:%s:function:*\"", var.trust_accounts))}
+          ]
+        }
+      }
     }
   ]
 }
